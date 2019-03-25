e1a5b9c46fcf5bd35d1a080e4ef4e66da2e3f0d8
Elijah Prince <elijahprince73@gmail.com>
2019-03-24 22:35:43 -0700

    creating one-many database

--- mysql-commands.sql
+++ mysql-commands.sql
@@ -1,70 +1,14 @@
--- SELECT
---  CONCAT(author_fname, ' ', author_lname) 
---  AS 'Full Name ' FROM books;
+CREATE TABLE customers(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(100),
+  last_name VARCHAR(100),
+  email VARCHAR(100)
+);
 
---  SELECT 
---     CONCAT_WS('-' , author_fname, author_lname)
---  AS 'Full Name ' FROM books;
-
--- SELECT SUBSTRING(title, 1, 10)
-
--- SELECT CONCAT(SUBSTRING(title, 1,10), ' ...') AS 'Short Title' FROM books;
-
--- SELECT REPLACE(title, 'e', 3) FROM books;
-
--- SELECT REVERSE(author_fname) FROM books;
-
--- SELECT CONCAT('woof', REVERSE('woof'));
-
--- SELECT author_lname, CHAR_LENGTH( author_lname) FROM books;
-
--- SELECT UPPER(title)
--- FROM books;
-
--- SELECT UPPER(REVERSE("Why does my cat look at me with such hatred?"));
-
--- SELECT
---     REPLACE
---   (
---   CONCAT('I', ' ', 'like', ' ', 'cats'),
---   ' ',
---   '-'
---   );
-
--- SELECT REPLACE(title, ' ', '->') from books;
-
--- SELECT author_fname AS 'FORWARDS', REVERSE(author_lname) AS 'BACKWARDS' FROM books;
-
--- SELECT UPPER(CONCAT
--- (author_fname, ' ',author_lname)) AS 'Full Name in caps' FROM books;
-
--- SELECT CONCAT(title, ' was released in ', released_year) FROM books;
-
--- SELECT title, CHAR_LENGTH(Title) FROM books;
-
--- SELECT CONCAT(SUBSTRING(title, 1, 10), '...') AS 'Short Title', CONCAT(author_fname,',' ,author_lname) AS 'Author', CONCAT(stock_quantity, ' in stock') from books;
-
--- SELECT DISTINCT author_lname from books;
--- SELECT DISTINCT CONCAT(author_fname,' ', author_lname) FROM books
-
--- SELECT author_lname FROM books ORDER BY author_lname
--- SELECT author_lname FROM books ORDER BY author_lname DESC
-
--- SELECT title, pages FROM books ORDER BY released_year;
-
--- SELECT title, released_year FROM books ORDER BY released_year LIMIT 5;
-
--- SELECT title, author_fname FROM books WHERE author_fname LIKE '%da%';
-
--- SELECT title, stock_quantity FROM books WHERE stock_quantity LIKE '____';
-
--- SELECT title from books where title LIKE '%stories%';
-
--- SELECT title, pages FROM books ORDER BY pages DESC LIMIT 1;
-
--- SELECT CONCAT(title, ' - ',released_year) AS summary FROM books
--- ORDER BY released_year DESC Limit 3;
-
--- SELECT title, author_lname FROM books WHERE author_lname LIKE "% %";
-
--- SELECT title, released_year, stock_quantity FROM books ORDER BY stock_quantity LIMIT 3;
+CREATE TABLE orders(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    order_date DATE,
+    amount DECIMAL(8,2),
+    customer_id INT,
+    FOREIGN KEY(customer_id) REFERENCES customers(id)
+);
