b8390ec3eb7e0d0f7f2e8e5e9edfa42cf7d5f35a
Elijah Prince <elijahprince73@gmail.com>
2019-04-02 21:45:10 -0700

    learning many to many

--- mysql-commands.sql
+++ mysql-commands.sql
@@ -1,71 +1,105 @@
--- CREATE TABLE customers(
---   id INT AUTO_INCREMENT PRIMARY KEY,
---   first_name VARCHAR(100),
---   last_name VARCHAR(100),
---   email VARCHAR(100)
--- );
+-- CREATE DATABASE show_review;
 
--- CREATE TABLE orders(
+-- CREATE TABLE reviewers(
 --     id INT AUTO_INCREMENT PRIMARY KEY,
---     order_date DATE,
---     amount DECIMAL(8,2),
---     customer_id INT,
---     FOREIGN KEY(customer_id) REFERENCES customers(id)
+--     first_name VARCHAR(100),
+--     last_name VARCHAR(100)
 -- );
 
--- SELECT * FROM orders WHERE customer_id = (
---     SELECT id
--- FROM customers
--- WHERE last_name='George'
+-- CREATE TABLE series(
+--     id INT AUTO_INCREMENT PRIMARY KEY,
+--     title VARCHAR(100),
+--     released_year YEAR(4),
+--     genre VARCHAR(100)
 -- )
 
--- SELECT * FROM customers, orders WHERE customers.id = orders.customer_id
 
--- SELECT * FROM customers
--- JOIN orders
---     ON customers.id = orders.customer_id;
-
--- SELECT *
--- FROM customers
---     RIGHT JOIN orders
---     ON customers.id = orders.customer_id;
-
--- CREATE TABLE students(
+-- CREATE TABLE reviews(
 --     id INT AUTO_INCREMENT PRIMARY KEY,
---     firstName VARCHAR(200)
--- );
+--     rating DECIMAL(2,1),
+--     series_id INT,
+--     reviewer_id INT,
+--     FOREIGN KEY(series_id) REFERENCES series(id),
+--     FOREIGN KEY (reviewer_id) REFERENCES reviewers(id)
+-- )
+-- INSERT INTO series
+--     (title, released_year, genre)
+-- VALUES`
+--     ('Archer', 2009, 'Animation'),
+--     ('Arrested Development', 2003, 'Comedy'),
+--     ("Bob's Burgers", 2011, 'Animation'),
+--     ('Bojack Horseman', 2014, 'Animation'),
+--     ("Breaking Bad", 2008, 'Drama'),
+--     ('Curb Your Enthusiasm', 2000, 'Comedy'),
+--     ("Fargo", 2014, 'Drama'),
+--     ('Freaks and Geeks', 1999, 'Comedy'),
+--     ('General Hospital', 1963, 'Drama'),
+--     ('Halt and Catch Fire', 2014, 'Drama'),
+--     ('Malcolm In The Middle', 2000, 'Comedy'),
+--     ('Pushing Daisies', 2007, 'Comedy'),
+--     ('Seinfeld', 1989, 'Comedy'),
+--     ('Stranger Things', 2016, 'Drama');
 
--- CREATE TABLE papers(
---     title VARCHAR(100),
---     grade VARCHAR(1),
---     student_id INT,
---     FOREIGN KEY(student_id) REFERENCES students(id)
--- );
 
--- INSERT INTO students
---     (firstName)
+-- INSERT INTO reviewers
+--     (first_name, last_name)
 -- VALUES
---     ('Caleb'),
---     ('Samantha'),
---     ('Raj'),
---     ('Carlos'),
---     ('Lisa');
+--     ('Thomas', 'Stoneman'),
+--     ('Wyatt', 'Skaggs'),
+--     ('Kimbra', 'Masters'),
+--     ('Domingo', 'Cortes'),
+--     ('Colt', 'Steele'),
+--     ('Pinkie', 'Petit'),
+--     ('Marlon', 'Crafford');
 
--- INSERT INTO papers
---     (student_id, title, grade )
+
+-- INSERT INTO reviews
+--     (series_id, reviewer_id, rating)
 -- VALUES
---     (1, 'My First Book Report', 'F'),
---     (1, 'My Second Book Report', 'C'),
---     (2, 'Russian Lit Through The Ages', 'A'),
---     (2, 'De Montaigne and The Art of The Essay', 'A'),
---     (4, 'Borges and Magical Realism', "B");
-    
--- SELECT * FROM students 
---     LEFT JOIN papers
---         ON students.id = papers.student_id
-
-SELECT firstName, IFNULL(title, 'missing'), IFNULL
-(grade, 'F')
-FROM students
-    LEFT JOIN papers
-    ON students.id = papers.student_id    
+--     (1, 1, 8.0),
+--     (1, 2, 7.5),
+--     (1, 3, 8.5),
+--     (1, 4, 7.7),
+--     (1, 5, 8.9),
+--     (2, 1, 8.1),
+--     (2, 4, 6.0),
+--     (2, 3, 8.0),
+--     (2, 6, 8.4),
+--     (2, 5, 9.9),
+--     (3, 1, 7.0),
+--     (3, 6, 7.5),
+--     (3, 4, 8.0),
+--     (3, 3, 7.1),
+--     (3, 5, 8.0),
+--     (4, 1, 7.5),
+--     (4, 3, 7.8),
+--     (4, 4, 8.3),
+--     (4, 2, 7.6),
+--     (4, 5, 8.5),
+--     (5, 1, 9.5),
+--     (5, 3, 9.0),
+--     (5, 4, 9.1),
+--     (5, 2, 9.3),
+--     (5, 5, 9.9),
+--     (6, 2, 6.5),
+--     (6, 3, 7.8),
+--     (6, 4, 8.8),
+--     (6, 2, 8.4),
+--     (6, 5, 9.1),
+--     (7, 2, 9.1),
+--     (7, 5, 9.7),
+--     (8, 4, 8.5),
+--     (8, 2, 7.8),
+--     (8, 6, 8.8),
+--     (8, 5, 9.3),
+--     (9, 2, 5.5),
+--     (9, 3, 6.8),
+--     (9, 4, 5.8),
+--     (9, 6, 4.3),
+--     (9, 5, 4.5),
+--     (10, 5, 9.9),
+--     (13, 3, 8.0),
+--     (13, 4, 7.2),
+--     (14, 2, 8.5),
+--     (14, 3, 8.9),
+--     (14, 4, 8.9);
