ce0753ee7ecaab007f5e837e0aa7594a193d00c2
Elijah Prince <elijahprince73@gmail.com>
2019-04-10 21:46:39 -0700

    practising ig data

--- mysql-commands.sql
+++ mysql-commands.sql
@@ -41,15 +41,68 @@
 --     PRIMARY KEY (followee_id, follower_id)
 -- )
 
-CREATE TABLE tags (
-  id INTEGER AUTO_INCREMENT PRIMARY KEY,
-  tag_name VARCHAR(255) UNIQUE,
-  created_at TIMESTAMP DEFAULT NOW()
-);
-CREATE TABLE photo_tags (
-    photo_id INTEGER NOT NULL,
-    tag_id INTEGER NOT NULL,
-    FOREIGN KEY(photo_id) REFERENCES photos(id),
-    FOREIGN KEY(tag_id) REFERENCES tags(id),
-    PRIMARY KEY(photo_id, tag_id)
-);
+-- CREATE TABLE tags (
+--   id INTEGER AUTO_INCREMENT PRIMARY KEY,
+--   tag_name VARCHAR(255) UNIQUE,
+--   created_at TIMESTAMP DEFAULT NOW()
+-- );
+-- CREATE TABLE photo_tags (
+--     photo_id INTEGER NOT NULL,
+--     tag_id INTEGER NOT NULL,
+--     FOREIGN KEY(photo_id) REFERENCES photos(id),
+--     FOREIGN KEY(tag_id) REFERENCES tags(id),
+--     PRIMARY KEY(photo_id, tag_id)
+-- );
+
+-- FINDING THE OLDEST USERS
+-- SELECT * FROM users
+-- ORDER BY created_at LIMIT 5;
+
+-- MOST POPULAR REGISTRATION DATA
+-- SELECT 
+--     DAYNAME(created_at) AS day,
+--     COUNT(*) AS total
+-- FROM users
+-- GROUP BY day
+-- ORDER BY total DESC
+-- LIMIT 2;
+
+-- SELECT username
+--  FROM users
+-- LEFT JOIN photos
+--   ON users.id = photos.user_id
+-- WHERE photos.id IS NULL;
+
+-- FIND MOST POPULAR PHOTO
+-- SELECT 
+--   username,
+--   photos.id, 
+--   photos.image_url, 
+--   COUNT(*) AS total
+-- FROM photos
+-- INNER JOIN likes
+--   ON likes.photo_id = photos.id
+-- INNER JOIN users  
+--   ON photos.user_id = users.id
+-- GROUP BY photos.id
+-- ORDER BY total DESC
+-- LIMIT 1;
+
+-- Average amount of posts
+-- SELECT 
+-- (SELECT COUNT(*) FROM photos) / (SELECT COUNT(*) FROM users)
+
+-- Most popular hashtags
+-- SELECT tags.tag_name, COUNT(*) AS total FROM photo_tags
+-- JOIN tags
+--   ON photo_tags.tag_id = tags.id
+-- GROUP BY tags.id
+-- ORDER BY total DESC
+-- LIMIT 5
+
+-- Finding bots
+SELECT username, COUNT(*) AS total FROM users 
+INNER JOIN likes
+  ON users.id = likes.user_id
+GROUP BY likes.user_id
+HAVING total = (SELECT COUNT(*) FROM photos)
